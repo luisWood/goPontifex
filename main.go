4c9d146e407fa2fc03d0a4b28388a5f7db0cebec
Luis Wood <luiswood@L-XPQRQ9NJ41.local>
2024-12-11 19:24:39 +0000

    Fixed code

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 )
@@ -109,8 +110,15 @@ func countCut(deck []int, deckSize int) []int {
 }
 
 func getKeystream(deck []int) int {
-	keystream := deck[deck[0]]
-	return keystream
+	// keystream := -1
+
+	if deck[0] == len(deck) && deck[len(deck)-1] <= len(deck)-1 {
+		return deck[len(deck)-1]
+	} else if deck[0] == len(deck) && deck[len(deck)-1] >= len(deck)-1 {
+		return -1
+	} else {
+		return deck[deck[0]]
+	}
 }
 
 func numericToAlphabetic(numericMessage []int) []string {
@@ -129,6 +137,7 @@ func numericToAlphabetic(numericMessage []int) []string {
 func alphabeticToNumeric(message string) []int {
 	results := []int{}
 	result := 0
+	message = strings.ToLower(message)
 	for _, char := range message {
 		if char < 'a' || char > 'z' {
 			continue // Skip non-alphabetic characters
@@ -140,5 +149,129 @@ func alphabeticToNumeric(message string) []int {
 	return results
 }
 
+func encrypt(numericMessage []int) struct {
+	message   []int
+	deck      []int
+	keystream []int
+} {
+	DECK_SIZE := 10
+	JOKER_A_VALUE := DECK_SIZE - 1
+	JOKER_B_VALUE := DECK_SIZE
+	initialDeck := setupDeck(DECK_SIZE)
+
+	deck := make([]int, len(initialDeck))
+	copy(deck, initialDeck)
+
+	encryptedNumericMessage := []int{}
+	keystreamSlice := []int{}
+
+	for _, value := range numericMessage {
+		deck = moveJoker(JOKER_A_VALUE, 1, deck)
+		deck = moveJoker(JOKER_B_VALUE, 2, deck)
+		deck = tripleCut(deck, JOKER_A_VALUE, JOKER_B_VALUE)
+		deck = countCut(deck, DECK_SIZE)
+
+		keystream := -1
+		keystream = getKeystream(deck)
+		if keystream != -1 {
+			keystreamSlice = append(keystreamSlice, keystream)
+			encryptedNumericMessage = append(encryptedNumericMessage, value+keystream)
+		} else {
+			deck = moveJoker(JOKER_A_VALUE, 1, deck)
+			deck = moveJoker(JOKER_B_VALUE, 2, deck)
+			deck = tripleCut(deck, JOKER_A_VALUE, JOKER_B_VALUE)
+			deck = countCut(deck, DECK_SIZE)
+
+			keystream = getKeystream(deck)
+			keystreamSlice = append(keystreamSlice, keystream)
+			encryptedNumericMessage = append(encryptedNumericMessage, value+keystream)
+		}
+
+	}
+
+	result := struct {
+		message   []int
+		deck      []int
+		keystream []int
+	}{
+		encryptedNumericMessage,
+		initialDeck,
+		keystreamSlice,
+	}
+
+	return result
+}
+
+func decrypt(message []int, deck []int) struct {
+	message   []int
+	keystream []int
+} {
+	DECK_SIZE := len(deck)
+	JOKER_A_VALUE := DECK_SIZE - 1
+	JOKER_B_VALUE := DECK_SIZE
+
+	keystreamSlice := []int{}
+	decryptedNumericMessage := []int{}
+	for _, value := range message {
+		deck = moveJoker(JOKER_A_VALUE, 1, deck)
+		deck = moveJoker(JOKER_B_VALUE, 2, deck)
+		deck = tripleCut(deck, JOKER_A_VALUE, JOKER_B_VALUE)
+		deck = countCut(deck, DECK_SIZE)
+
+		// keystream := getKeystream(deck)
+
+		keystream := -1
+
+		keystream = getKeystream(deck)
+
+		if keystream != -1 {
+			keystreamSlice = append(keystreamSlice, keystream)
+			decryptedNumericMessage = append(decryptedNumericMessage, value-keystream)
+		} else {
+			deck = moveJoker(JOKER_A_VALUE, 1, deck)
+			deck = moveJoker(JOKER_B_VALUE, 2, deck)
+			deck = tripleCut(deck, JOKER_A_VALUE, JOKER_B_VALUE)
+			deck = countCut(deck, DECK_SIZE)
+
+			keystream = getKeystream(deck)
+			keystreamSlice = append(keystreamSlice, keystream)
+			decryptedNumericMessage = append(decryptedNumericMessage, value-keystream)
+		}
+
+		// keystreamSlice = append(keystreamSlice, keystream)
+	}
+
+	result := struct {
+		message   []int
+		keystream []int
+	}{
+		decryptedNumericMessage,
+		keystreamSlice,
+	}
+
+	return result
+}
+
 func main() {
+	example := "this is a test"
+	numericMessage := alphabeticToNumeric(example)
+
+	result := encrypt(numericMessage)
+	deck := result.deck
+
+	encryptedMessage := result.message
+	decryptedResult := decrypt(encryptedMessage, deck)
+	fmt.Println("keystream: ", decryptedResult)
+
+	decryptedNumericMessage := decryptedResult.message
+	decryptedMessage := numericToAlphabetic(decryptedNumericMessage)
+
+	fmt.Println("Original:", example)
+	fmt.Println("Encrypted:", numericToAlphabetic(encryptedMessage))
+	fmt.Println("Decrypted:", (decryptedMessage))
+	fmt.Println("Encryption Keystream:", result.keystream)
+	fmt.Println("Decryption Keystream:", decryptedResult.keystream)
+	// fmt.Println(numericMessage)
+	fmt.Println(result.keystream)
+	fmt.Println(decryptedResult.keystream)
 }
